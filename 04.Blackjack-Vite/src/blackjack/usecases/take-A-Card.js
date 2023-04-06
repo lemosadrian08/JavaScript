74decfbf1f564d3c970a1f8e6c1e55acfad8dc11
Adrian Lemos <lemosadrian08@gmail.com>
2023-04-05 23:45:40 -0300

    the code was refactored

---
+++ 04.Blackjack-Vite/src/blackjack/usecases/take-A-Card.js
@@ -0,0 +1,17 @@
+  
+  
+
+  
+/** 
+ * This function allows you to take a card
+ * @param {Array<String>} deck 
+ * @returns {String} returns a card 
+ */  
+
+
+  export const takeACard = (deck) =>{
+    if (!deck || deck.length === 0) {
+        throw new Error('There no cards on the deck')
+    }
+    return deck.pop();
+}
